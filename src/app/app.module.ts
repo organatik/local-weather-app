d6ed101135f0b4e8833f11ef7282606b0cdc4bd4
Ant_Ang <ant_ang@mail.com>
2020-08-18 11:36:57 +0300

    pre update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CurrentWeatherComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
